e30b4bba10e67266924d679c042e973d6e969638
ShevenAndrey <himynameisandrey@gmail.com>
2023-03-13 00:44:23 +0300

    Правки задания №3

--- test_homework_6.py
+++ test_homework_6.py
@@ -1,5 +1,6 @@
 from datetime import time
 
+
 def test_dark_theme():
     """
     Протестируйте правильность переключения темной темы на сайте
@@ -53,10 +54,10 @@ def test_find_suitable_user():
 # >>> open_browser(browser_name="Chrome")
 # "Open Browser [Chrome]"
 
-def print_func(func):
-    name_func = ' '.join(func.__name__.title().split('_'))
-    arg_func = 'ДОДЕЛАТЬ'
-    return (f'{name_func} [{arg_func}]')
+def print_func(func, *args):
+    name = ' '.join(func.__name__.title().split('_'))
+    arguments = ', '.join(args)
+    return f'{name} [{arguments}]'
 
 
 def test_readable_function():
@@ -65,13 +66,13 @@ def test_readable_function():
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 def open_browser(browser_name):
-    actual_result = print_func(open_browser)
+    actual_result = print_func(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 def go_to_companyname_homepage(page_url):
-    actual_result = print_func(go_to_companyname_homepage)
+    actual_result = print_func(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = print_func(find_registration_button_on_login_page)
+    actual_result = print_func(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
